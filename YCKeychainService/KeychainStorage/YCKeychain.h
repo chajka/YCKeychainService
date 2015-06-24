46366d9c781433dbb7cf6785cc8a95326c35bf8e
Чайка <mellanie335@gmail.com>
2015-06-24 14:22:40 +0900

    Implement class YCKeychain.

---
+++ YCKeychainService/KeychainStorage/YCKeychain.h
@@ -0,0 +1,13 @@
+//
+//  YCKeychain.h
+//  YCKeychainService
+//
+//  Created by Чайка on 6/24/15.
+//  Copyright (c) 2015 Instrumentality of Mankind. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YCKeychain : NSObject {
+}
+@end
---
+++ YCKeychainService/KeychainStorage/YCKeychain.m
@@ -0,0 +1,13 @@
+//
+//  YCKeychain.m
+//  YCKeychainService
+//
+//  Created by Чайка on 6/24/15.
+//  Copyright (c) 2015 Instrumentality of Mankind. All rights reserved.
+//
+
+#import "YCKeychain.h"
+
+@implementation YCKeychain
+
+@end
