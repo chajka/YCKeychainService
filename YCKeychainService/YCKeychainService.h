e19065c707f796ef50822362312a03b870673a31
Чайка <mellanie335@gmail.com>
2015-06-24 13:17:10 +0900

    Initial Commit

---
+++ YCKeychainService/YCKeychainService.h
@@ -0,0 +1,19 @@
+//
+//  YCKeychainService.h
+//  YCKeychainService
+//
+//  Created by Чайка on 6/24/15.
+//  Copyright (c) 2015 Instrumentality of Mankind. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for YCKeychainService.
+FOUNDATION_EXPORT double YCKeychainServiceVersionNumber;
+
+//! Project version string for YCKeychainService.
+FOUNDATION_EXPORT const unsigned char YCKeychainServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YCKeychainService/PublicHeader.h>
+
+
