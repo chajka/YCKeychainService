59628673bcefc3d1f96e25908bd7d912bdef9fc6
Чайка <mellanie335@gmail.com>
2015-06-26 14:48:26 +0900

    Add import all headers to YCKeychainService.h

---
+++ YCKeychainService/GenericKeychainItem/YCGenericKeychainItem.h
@@ -0,0 +1,13 @@
+//
+//  YCGenericKeychainItem.h
+//  YCKeychainService
+//
+//  Created by Чайка on 6/26/15.
+//  Copyright (c) 2015 Instrumentality of Mankind. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YCGenericKeychainItem : NSObject
+
+@end
---
+++ YCKeychainService/GenericKeychainItem/YCGenericKeychainItem.m
@@ -0,0 +1,24 @@
+//
+//  YCGenericKeychainItem.m
+//  YCKeychainService
+//
+//  Created by Чайка on 6/26/15.
+//  Copyright (c) 2015 Instrumentality of Mankind. All rights reserved.
+//
+
+#import "YCGenericKeychainItem.h"
+
+@implementation YCGenericKeychainItem
+#pragma mark - synthesize properties
+#pragma mark - class method
+#pragma mark - constructor / destructor
+#pragma mark - override
+#pragma mark - delegate
+#pragma mark - properties
+#pragma mark - actions
+#pragma mark - messages
+#pragma mark - private
+#pragma mark - C functions
+
+
+@end
